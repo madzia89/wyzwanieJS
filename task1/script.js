7d979f075b3c8c3c60e838e2088e3de8ecd9da33
madzia89 <wojcicka.ma@gmail.com>
2018-04-05 15:17:29 +0200

    first task typeof

---
+++ task1/script.js
@@ -0,0 +1,9 @@
+var magda = "Magda"
+var someNumber = 15
+var someArray = [1,2,3]
+var something = true
+
+console.log("magda", magda, typeof magda)
+console.log("someNumber", someNumber, typeof someNumber)
+console.log("someArray", someArray, typeof someArray)
+console.log("something", something, typeof something)
