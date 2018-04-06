14a3de61ae5ae07062918fc49bddb00fc985fba6
madzia89 <wojcicka.ma@gmail.com>
2018-04-06 09:55:41 +0200

    unary plus, increment, decrement)

---
+++ task2/script.js
@@ -0,0 +1,16 @@
+var age1 = 25
+var age2= 20
+var age3= 19
+var age4= 22
+var age5 = 26
+var sum = age1 + age2 + age3 + age4 + age5
+console.log(sum)
+var avarage = sum / 5
+console.log(avarage)
+var even = sum % 2
+console.log(even)
+if (even == 0){
+    console.log('yay! it is an even number!')
+} else {
+    console.log ('no, it is not an even number')
+}
---
+++ task3/script.js
@@ -0,0 +1,15 @@
+var a = 3
+var b = "3"
+ console.log (a === b)
+
+var c = 4
+var d = "4"
+
+console.log(c !== d)
+var d = +d //changes d into a number although it was a string before)
+console.log(typeof d)
+
+console.log (+true) //returns 1
+console.log(typeof +true) //number because it's 1!
+console.log (+null)//returns 0
+console.log(typeof +null)//returns number because it's 0 !
