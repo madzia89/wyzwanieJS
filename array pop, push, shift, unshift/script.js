d4b91c70ff24f0be82ae209414b600ec63bb644d
madzia89 <wojcicka.ma@gmail.com>
2018-04-06 14:46:31 +0200

    arrays push, pop, shift, unshift

---
+++ array pop, push, shift, unshift/script.js
@@ -0,0 +1,19 @@
+var newArr = [1,8,7,5,6]
+
+newArr.unshift(0)           //adds 0 at the beginning
+console.log(newArr)
+// [0, 1, 8, 7, 5, 6]
+
+
+newArr.push(6)              //adds 6 at the end
+console.log(newArr)
+//[0, 1, 8, 7, 5, 6, 6]
+
+
+newArr.shift()              //removes first element of an array
+console.log(newArr)
+//[1, 8, 7, 5, 6, 6]
+
+newArr.pop()                //remowes last element of an array
+console.log(newArr)
+//[1, 8, 7, 5, 6]
