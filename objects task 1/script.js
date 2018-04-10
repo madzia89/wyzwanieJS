7ddc38f41cdf8fe10f124f9bbe60fb481fcdb017
madzia89 <wojcicka.ma@gmail.com>
2018-04-10 10:39:34 +0200

    objects task 1

---
+++ objects task 1/script.js
@@ -0,0 +1,20 @@
+var myCar = new Object ()
+
+myCar.year = 2000
+myCar.model = "volvo"
+myCar.make = "v40"
+
+console.log (myCar)
+
+
+// stwórz objekt. W tym przypadku obiektem jest myCar a właściościami (properties) są pozostałe dane. można to zapisać również w inny sposób:
+
+var myCar2 = {
+
+    make : "volvo",
+    model: "v40",
+    year: 2000,
+}
+console.log(myCar2)
+
+//dwa powyższe sobosby w tej sytuacji niczym się nie różnią
