274f310f2dcaabb6bc9472c6fbd75af6887d2248
madzia89 <wojcicka.ma@gmail.com>
2018-04-10 12:17:23 +0200

    IIFE

--- functions counter exercise/script.js
+++ functions counter exercise/script.js
@@ -3,10 +3,9 @@ var counter = 0
 
        counter ++
         console.log("ta funkcja jest wywołana po raz " + counter + " ")
-
     }
 
-//     name()
+// name()
 // name()
 // name()                                  /// wykona funkcję tyle razy ile razy ją wywołamy lub można wywołać ją pentlą tak jak poniżej
 // name()
@@ -18,3 +17,5 @@ var counter = 0
 for (var i = 0; i < 10; i ++) {
     name()
 }
+
+//funkcja ma się wykonywać tak długo aż i nie będzie mniejsze od 10!
---
+++ functions counter exercise2/script.js
@@ -0,0 +1,17 @@
+
+(function(){
+var counter = 0
+    function name() {
+
+       counter ++
+        console.log("ta funkcja jest wywołana po raz " + counter + " ")
+    }
+for (var i = 0; i < 10; i ++) {
+    name()
+}
+})()
+
+console.log(counter)
+
+
+//użycie IIFE funkcji z zadania functons1 console log nie może się odwołać do counter bo jest to lokalna zmienna - znajduje sie w funkcji do której console.log nie ma dostępu
